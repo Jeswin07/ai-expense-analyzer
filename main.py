1c1d5e6de3a38a37a255fb3279cedfc13361604f
Jeswin <jeswinkr7@gmail.com>
2026-05-20 02:49:26 +0300

    UPDATE CORS

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "https://ai-expense-analyzer-tau.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
